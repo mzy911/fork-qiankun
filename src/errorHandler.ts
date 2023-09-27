47686d8dedb46a9ae4e4f6883dc3fbca377edf28
孟凡荣 <mfr01606587@alibaba-inc.com>
2023-09-27 18:24:37 +0800

    熟悉代码

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -3,13 +3,17 @@
  * @since 2020-02-21
  */
 
+// single-spa 的异常捕获
 export { addErrorHandler, removeErrorHandler } from 'single-spa';
 
+// qiankun 的异常捕获
+// 监听了 error 和 unhandlerejection 事件
 export function addGlobalUncaughtErrorHandler(errorHandler: OnErrorEventHandlerNonNull): void {
   window.addEventListener('error', errorHandler);
   window.addEventListener('unhandledrejection', errorHandler);
 }
 
+// 移除 error 和 unhandlerejection 事件监听
 export function removeGlobalUncaughtErrorHandler(errorHandler: (...args: any[]) => any) {
   window.removeEventListener('error', errorHandler);
   window.removeEventListener('unhandledrejection', errorHandler);
